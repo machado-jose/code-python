d3255e512f11fd01a9285d1fe9dc17a34decf5e6
machado-jose <machado.josegustavo@gmail.com>
2020-04-11 20:28:30 -0300

    Adicionando o script nested_list

---
+++ list/nested_lists.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+
+    students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+    #for _ in range(int(input())):
+        #name = input()
+        #score = float(input())
+        #students.append([name, score])
+
+
+    min_score = min(list(map(lambda x: x[1], students)))
+    new_list_students = [students[i] for i in range(len(students)) if min_score != students[i][1]]
+
+    min_score = min(list(map(lambda x: x[1], new_list_students)))
+
+    for student in sorted(new_list_students):
+        if student[1] == min_score:
+            print(student[0])
